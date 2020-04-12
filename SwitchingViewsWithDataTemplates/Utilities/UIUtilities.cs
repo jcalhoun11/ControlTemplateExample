fcb2b9a168647dedf700a81f5eab0aa782a855af
jcalhoun <jcalhoun1108@gmail.com>
2020-04-12 12:02:12 -0500

    commit
    commit

---
+++ SwitchingViewsWithDataTemplates/Utilities/UIUtilities.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SwitchingViewsWithDataTemplates.Utilities
+{
+    public class ExecutePath : ICommand
+    {
+        #region fields
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+        #endregion
+
+        #region functions
+        public ExecutePath(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        public ExecutePath(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameters)
+        {
+            return _canExecute == null ? true : _canExecute(parameters);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameters)
+        {
+            _execute(parameters);
+        }
+        #endregion
+    }
+
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnChanged([CallerMemberName]string prop = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ SwitchingViewsWithDataTemplates/ViewModels/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using SwitchingViewsWithDataTemplates.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SwitchingViewsWithDataTemplates.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ViewOneViewModel _ViewOneViewModel;
+        private ViewTwoViewModel _ViewTwoViewModel;
+
+        private object _ControlContext;
+
+        public object ControlContext
+        {
+            get { return _ControlContext; }
+            set { _ControlContext = value; OnChanged(); }
+        }
+
+
+        private ICommand _SwitchToViewOneCommand;
+        public ICommand SwitchToViewOneCommand
+        {
+            get
+            {
+                if (_SwitchToViewOneCommand == null)
+                    _SwitchToViewOneCommand = new ExecutePath(param => SwitchToViewOne(param));
+
+                return _SwitchToViewOneCommand;
+            }
+        }
+
+        private ICommand _SwitchToViewTwoCommand;
+        public ICommand SwitchToViewTwoCommand
+        {
+            get
+            {
+                if (_SwitchToViewTwoCommand == null)
+                    _SwitchToViewTwoCommand = new ExecutePath(param => SwitchToViewTwo(param));
+
+                return _SwitchToViewTwoCommand;
+            }
+        }
+
+        public MainViewModel()
+        {
+            _ViewOneViewModel = new ViewOneViewModel();
+            _ViewTwoViewModel = new ViewTwoViewModel();
+        }
+
+        private void SwitchToViewTwo(object param)
+        {
+            ControlContext = _ViewTwoViewModel;
+        }
+
+        private void SwitchToViewOne(object param)
+        {
+            ControlContext = _ViewOneViewModel;
+        }
+
+
+    }
+}
